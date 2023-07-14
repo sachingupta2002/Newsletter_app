23c49c7102a914e8948236abc15146af6bcaa443
sachingupta2002 <125809897+sachingupta2002@users.noreply.github.com>
2023-07-14 12:45:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require("express");
+
+const https = require("https");
+const request =require("request");
+const bodyParser = require("body-parser");
+const app= new express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.get('/',function(req,res)
+{
+    
+    res.sendFile(__dirname+"/signup.html");
+});
+
+app.post("/",function(req,res)
+{
+    const firstName=req.body.fname;
+    const lastName =req.body.lname;
+    const email= req.body.email;
+
+   const data={
+    members:[
+        {
+            email_address:email,
+            status:"subscribed",
+            merge_fields:{
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }
+    ]
+   };
+   const jsonData =JSON.stringify(data);
+
+   const url="https://us8.api.mailchimp.com/3.0/lists/aa9df69c46";
+
+   const options ={
+    method:"POST",
+    auth:"sachin:c18961fce29042d34a4835d9f8585e75-us8"
+
+   }
+
+   const request=https.request(url,options,function(response)
+    {
+        if(response.statusCode===200)
+        {
+            res.sendFile(__dirname+"/success.html");
+        }
+        else{
+            res.sendFile(__dirname+"/failure.html");
+        }
+        response.on("data",function(data)
+        {
+            console.log(JSON.parse(data));
+        });
+    });
+    request.write(jsonData);
+    request.end();
+});
+
+app.post("/failure",function(req,res)
+{
+    res.redirect("/");
+});
+
+app.listen(process.env.PORT||3000,function()
+{
+    console.log("Server is running on 3000");
+});
+
+//c18961fce29042d34a4835d9f8585e75-us8
+//aa9df69c46
